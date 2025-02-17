c2822f370ce84150c3516c9a3809a3a68fe21849
Natalie Devenbeck <devenbeck@usf.edu>
2025-02-16 21:02:27 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal, taxRate, discount) {
 }; 
 console.log(calculateInvoice(100, 0.08 ,5)); //for a 100 subtotal at an 8% tax with a 5 dollar discount output should be $103
 console.log(calculateInvoice(500, 0.1 , 20)); 
+
+// Task 2 : Function Expression
+//Scenario: Employee Hourly Wage Calculation
+function calculateHourlyWage(salary, hoursPerWeek) {
+    const hourlyWage = (salary / (hoursPerWeek * 52)); //formula given
+    return `Hourly wage: $${hourlyWage.toFixed(2)}`; //same as above just different formula
+};
+console.log(calculateHourlyWage(52000, 40));  //should return $25.00
+console.log(calculateHourlyWage(75000, 35));
